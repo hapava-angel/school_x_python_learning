4d8811e2d70532b65e76c8cc30d5c4be03b4c00d
Angelina Khapava <a.khapava.sas@gmail.com>
2023-09-08 09:39:26 +0300

    fgh

---
+++ lecture_4/module.py
@@ -0,0 +1,5 @@
+def greet(name: str) -> str:
+
+    return f'Hello {name}'
+if __name__ == '_main_':
+    print('chto-to')
---
+++ lecture_4/scope.py
@@ -0,0 +1,23 @@
+# z=12
+# def outer(y):
+#     print(f'y in answer: {"y" in locals()}')
+#     # y = 12
+#     # print(f'y in outer after assing: {"y" in locals()}')
+#     def answer():
+        
+#         print(locals())
+#     print(globals())
+#     return answer()
+
+# if __name__ == '_main_':
+#     x = 42
+#     print(f'y in main is exist: {"y" in locals()}')
+#     outer(10)
+
+alphabet ={
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'X',
+    10: 'Y',
+}
